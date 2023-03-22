74555d13fe584bc8a099d8d9499fcc9e4f934e94
kasparia <kasperi.koskelo@kodan.fi>
2023-03-22 23:43:35 +0200

    Add simplified registration

--- website/views.py
+++ website/views.py
@@ -1,27 +1,34 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, logout_user, current_user
 from . import db, is_valid_email, is_valid_string
-from .models import Registrant
+from .models import Registrant, Visitor
 
 views = Blueprint('views', __name__)
 
 @views.route('/', methods=['GET', 'POST'])
 def home():
 	
-	if request.args.get('code') !=None and request.args.get('email') !=None:
-		input_code = request.args.get('code')
-		input_email = request.args.get('email')
+	# if request.args.get('code') !=None and request.args.get('email') !=None:
+	if request.args.get('nameField') !=None and request.args.get('emailField') !=None:
+		# input_code = request.args.get('code')
+		nameFieldData = request.args.get('nameField')
+		emailFieldData = request.args.get('emailField')
 
-		if is_valid_email(input_email):
-			email = Registrant.query.filter_by(reg_email=input_email).first()
-			if email != None:
+		if is_valid_email(emailFieldData):
+			visitorEmail = Visitor.query.filter_by(visitorEmail=emailFieldData).first()
+			if visitorEmail != None:
 				flash("Email already registered", category='error')
-				return render_template("register.html", user=current_user, code=input_code)
+				# return render_template("register.html", user=current_user, code=input_code)
+				return render_template("register.html", user=current_user)
 			else:	
-				code = Registrant.query.filter_by(invitecode=input_code+"\n").first()
-				if code:					
-					code.reg_email = input_email
-					code.used = True
+				# visitorInstance = Visitor.query.filter_by(visitorEmail=emailFieldData+"\n").first()
+				#if is_valid_string(nameFieldData):		
+				visitorInstance = Visitor.query.filter_by(visitorEmail=emailFieldData+"\n").first()
+				if visitorInstance == None:
+					visitorInstance = Visitor()
+					visitorInstance.visitorName = nameFieldData
+					visitorInstance.visitorEmail = emailFieldData
+					db.session.add(visitorInstance)
 					db.session.commit()
 					return("Email registered!")					
 				else:
@@ -30,10 +37,15 @@ def home():
 						
 		else:
 			flash("Invalid email address", category='error')
-			return render_template("register.html", user=current_user, code=input_code)
+			# return render_template("register.html", user=current_user, code=input_code)
+			return render_template("register.html", user=current_user)
+	else:
+		#flash("Invalid code", category='error')
+		return render_template("home.html", user=current_user)
 
 
-	elif request.args.get('code') !=None:
+
+	"""elif request.args.get('code') !=None:
 		input_code = request.args.get('code').upper()
 		if is_valid_string(input_code):
 			code = Registrant.query.filter_by(invitecode=input_code+"\n").first()
@@ -52,14 +64,15 @@ def home():
 			flash("Invalid code", category='error')
 			return render_template("home.html", user=current_user)	
 	else:
-		return render_template("home.html", user=current_user)
+		return render_template("home.html", user=current_user)"""
 
 @views.route('/admin', methods=['GET'])
 @login_required
 def admin():
 
 	#Registrants = Registrant.query.order_by(Registrant.id).all()
-	Registrants = Registrant.query.filter_by(used=True).order_by(Registrant.id).all()
-	return render_template("admin.html", registrants=Registrants, user=current_user)
+	# Visitors = Visitor.query.order_by(Visitor.id).all()
+	Visitors = Visitor.query.all()
+	return render_template("admin.html", registrants=Visitors, user=current_user)
 
 
