bf951e189b501b3a06d9a17e9eb2fd250e9771f0
kasparia <kasperi.koskelo@kodan.fi>
2023-03-25 14:01:10 +0200

    Clean code and improve messages

--- website/views.py
+++ website/views.py
@@ -27,52 +27,24 @@ def home():
 					visitorInstance.visitorEmail = emailFieldData
 					db.session.add(visitorInstance)
 					db.session.commit()
-					# return("Email registered!")
-
 					flash("Registration successful", category='success')
 					return render_template("home.html", user=current_user, visitorRegistered=True)
+				
 				else:
 					flash("Invalid name given", category='error')
 					return render_template("home.html", user=current_user, visitorRegistered=False)
 						
 		else:
 			flash("Invalid email address", category='error')
-			# return render_template("register.html", user=current_user, code=input_code)
-			#return render_template("register.html", user=current_user)
 			return render_template("home.html", user=current_user, visitorRegistered=False)
 	else:
-		flash("Please fill your info :)", category='error')
+		flash("Please fill your info :)", category='info')
 		return render_template("home.html", user=current_user, visitorRegistered=False)
 
-
-
-	"""elif request.args.get('code') !=None:
-		input_code = request.args.get('code').upper()
-		if is_valid_string(input_code):
-			code = Registrant.query.filter_by(invitecode=input_code+"\n").first()
-		else:
-			flash("Don't bother trying to h4ck m3.", category='warning')
-			return render_template("home.html", user=current_user) 
-
-		if code:
-			if code.used == False:
-				flash("Code correct!", category='success')
-				return render_template("register.html", user=current_user, code=code.invitecode)
-			else:
-				flash("Code already registered!", category='error') 
-				return redirect(url_for('views.home'))
-		else:
-			flash("Invalid code", category='error')
-			return render_template("home.html", user=current_user)	
-	else:
-		return render_template("home.html", user=current_user)"""
 
 @views.route('/admin', methods=['GET'])
 @login_required
 def admin():
-
-	#Registrants = Registrant.query.order_by(Registrant.id).all()
-	# Visitors = Visitor.query.order_by(Visitor.id).all()
 	Visitors = Visitor.query.all()
 	return render_template("admin.html", registrants=Visitors, user=current_user)
 
