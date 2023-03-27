d8d17fac627b5ad0face846fa8b85dc433669125
kasparia <kasperi.koskelo@kodan.fi>
2023-03-27 11:55:22 +0300

    Add captcha functionality

--- website/views.py
+++ website/views.py
@@ -6,56 +6,65 @@ from captcha.image import ImageCaptcha
 
 views = Blueprint('views', __name__)
 
-@views.route('/', methods=['GET'])
+@views.route('/', methods=['GET', 'POST'])
 def home():
 	
-	if not 'captcha' in session:
-		generateCaptchaSequence()
+	if request.method == 'POST':
+		print("postt")
+		if request.form.get('nameField') != None and request.form.get('nameField') != "" and request.form.get('emailField') != None and request.form.get('emailField') != "" and request.form.get('codeTest') != "":
+			nameFieldData = request.form.get('nameField')
+			emailFieldData = request.form.get('emailField')
+			captchaData = request.form.get('codeTest')
 
-	if request.args.get('nameField') != None and request.args.get('nameField') != "" and request.args.get('emailField') != None and request.args.get('emailField') != "" and request.args.get('codeTest') != "":
-		nameFieldData = request.args.get('nameField')
-		emailFieldData = request.args.get('emailField')
-		captchaData = request.args.get('codeTest')
+			if captchaData != None and captchaData == session['captcha']:
 
-		if captchaData != None and captchaData == session['captcha']:
-
-			if is_valid_email(emailFieldData):
-				visitorEmail = Visitor.query.filter_by(visitorEmail=emailFieldData).first()
-				if visitorEmail != None:
-					flash("Email already registered", category='error')
-					return render_template("home.html", user=current_user, nameField=nameFieldData, emailField=emailFieldData)
-				
-				else:	
-					visitorInstance = Visitor.query.filter_by(visitorEmail=emailFieldData+"\n").first()
-
-					if visitorInstance == None and is_valid_string(nameFieldData):
-						visitorInstance = Visitor()
-						visitorInstance.visitorName = nameFieldData
-						visitorInstance.visitorEmail = emailFieldData
-						db.session.add(visitorInstance)
-						db.session.commit()
-						session.pop('captcha')
-						flash("Registration successful", category='success')
-						return render_template("home.html", user=current_user, visitorRegistered=True)
+				if is_valid_email(emailFieldData):
+					visitorEmail = Visitor.query.filter_by(visitorEmail=emailFieldData).first()
+					if visitorEmail != None:
+						flash("Email already registered", category='error')
+						captchaImage = generateCaptchaSequence()
+						return render_template("home.html", user=current_user, visitorRegistered=False, captchaImage=captchaImage)
 					
-					else:
-						flash("Invalid name given", category='error')
-						session.pop('captcha')
-						return render_template("home.html", user=current_user, visitorRegistered=False)
-							
+					else:	
+						visitorInstance = Visitor.query.filter_by(visitorEmail=emailFieldData+"\n").first()
+
+						if visitorInstance == None and is_valid_string(nameFieldData):
+							visitorInstance = Visitor()
+							visitorInstance.visitorName = nameFieldData
+							visitorInstance.visitorEmail = emailFieldData
+							db.session.add(visitorInstance)
+							db.session.commit()
+							session.pop('captcha')
+							flash("Registration successful", category='success')
+							return render_template("home.html", user=current_user, visitorRegistered=True)
+						
+						else:
+							flash("Invalid name given", category='error')
+							session.pop('captcha')
+							captchaImage = generateCaptchaSequence()
+							return render_template("home.html", user=current_user, visitorRegistered=False, captchaImage=captchaImage)
+								
+				else:
+					flash("Invalid email address", category='error')
+					session.pop('captcha')
+					captchaImage = generateCaptchaSequence()
+					return render_template("home.html", user=current_user, visitorRegistered=False, captchaImage=captchaImage)
 			else:
-				flash("Invalid email address", category='error')
-				session.pop('captcha')
-				return render_template("home.html", user=current_user, visitorRegistered=False)
+				flash("Please fill your info :)", category='info')
+				captchaImage = generateCaptchaSequence()
+				return render_template("home.html", user=current_user, visitorRegistered=False, captchaImage=captchaImage)
+
 		else:
 			flash("Please fill your info :)", category='info')
-			return render_template("home.html", user=current_user, visitorRegistered=False)
-
+			session.pop('captcha')
+			captchaImage = generateCaptchaSequence()
+			return render_template("home.html", user=current_user, visitorRegistered=False, captchaImage=captchaImage)
+	
 	else:
-		flash("Please fill your info :)", category='info')
-		session.pop('captcha')
-		return render_template("home.html", user=current_user, visitorRegistered=False)
-
+		print("gettt")
+		captchaImage = generateCaptchaSequence()
+		return render_template("home.html", user=current_user, visitorRegistered=False, captchaImage=captchaImage)
+	
 @views.route('/admin', methods=['GET'])
 @login_required
 def admin():
